fc122cf27fc58cdef2b127c6e0aad947501fe328
风起深空 <15827139224@163.com>
2025-09-17 22:41:52 +0800

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS  
+
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int x = 0;
+	int& r1 = x;
+	//指针和引用才涉及到权限的放大和缩小
+	const int y = 1;
+	//权限不能放大
+	//int& r2 = y;//要是这样的话虽然y不能被修改，但是r2是可以被取别名的。
+
+	const int& r2 = y;//这样的话r2也不可以修改了。
+
+
+	int z = 2;
+	int& r3 = z;
+	//权限可以缩小
+	const int& r4 = z;
+
+
+	// const int h = 3;  另外的一个表达形式 int const h=3;  语法上是一样的
+	//同理 const int& h=y;  等同于int const& h=y;
+	
+	int z = y;
+
+	const int* p1 = &y;
+	//权限不能放大
+	//int* p2 = p1;
+	const int* p2 = p1;
+
+	int* p3 = &z;
+	//权限可以缩小
+	const int* p4 = p3;
+	return 0;
+}
