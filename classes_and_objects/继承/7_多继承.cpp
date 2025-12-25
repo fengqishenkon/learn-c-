5e4f1b2baf0eb28fca29ce7edffd61c1415ec7bd
风起深空 <15827139224@163.com>
2025-12-25 20:48:52 +0800

    Add files via upload

---
+++ classes_and_objects/继承/7_多继承.cpp
@@ -0,0 +1,40 @@
+//#define _CRT_SECURE_NO_WARNINGS 
+//#include<iostream>
+//using namespace std;
+//
+//class father1
+//{
+//public:
+//	int a = 1000;
+//};
+//
+//
+//class father2
+//{
+//public:
+//	int a = 2000;
+//};
+//
+//class son :public father1, public father2
+//{
+//public:
+//	int c = 90;
+//};
+//
+//void test()
+//{
+//	son s;
+//	cout << "father1=" << s.a << endl;
+//	cout << "father1=" << s.father1::a << endl;
+//	cout << "father2=" << s.father2::a << endl;
+//
+//
+//}
+//
+//
+//int main()
+//{
+//
+//	test();
+//	return 0;
+//}
