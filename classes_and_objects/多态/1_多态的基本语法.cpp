98204ddd1f328f295f23db3e768ad2b06b97b472
风起深空 <15827139224@163.com>
2025-12-28 23:12:08 +0800

    Add files via upload

---
+++ classes_and_objects/多态/1_多态的基本语法.cpp
@@ -0,0 +1,39 @@
+//#define _CRT_SECURE_NO_WARNINGS 
+//#include<iostream>
+//using namespace std;
+//class Animal
+//{
+//public:
+//	virtual void speak()
+//	{
+//		cout << "动物说话" << endl;
+//	}
+//};
+//
+//class cat :public Animal
+//{
+//public:
+//	 void speak()
+//	{
+//		cout << "小猫说话" << endl;
+//	}
+//};
+//
+//void dospeak(Animal &animal)//Animal &animal=c;公有继承意味着“是一种”关系，cat是一种animal，所以cat对象可以当作animal对象使用。
+//{
+//	animal.speak();
+//}
+//
+//void test01()
+//{
+//	cat c;
+//	dospeak(c);
+//}
+//
+//
+//int main()
+//{
+//
+//	test01();
+//	return 0;
+//}
