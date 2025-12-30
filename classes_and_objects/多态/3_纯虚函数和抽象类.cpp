0dc004b5d9e05b9d194ea5c808fdb8f82cdb5afa
风起深空 <15827139224@163.com>
2025-12-30 18:25:31 +0800

    Add files via upload

---
+++ classes_and_objects/多态/3_纯虚函数和抽象类.cpp
@@ -0,0 +1,38 @@
+//#define _CRT_SECURE_NO_WARNINGS 
+//#include<iostream>
+//using namespace std;
+//
+////抽象类
+//class Base
+//{
+//public:
+//	virtual void function() = 0;
+//	void make()
+//	{
+//		function();
+//	}
+//};
+//
+//class Son :public Base
+//{
+//public:
+//	virtual void function()
+//	{
+//		cout << "重写" << endl;
+//	}
+//	int m_a;
+//};
+//
+////Base a;
+////Son a;
+//Son a;
+//
+////Base b;
+//int main()
+//{
+//	a.function();//在C++中，函数调用、循环、条件判断等执行语句必须放在函数体内：
+//
+//
+//	//Base::make;
+//	return 0;
+//}
