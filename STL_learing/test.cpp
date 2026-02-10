61d328ec7eee928703185385d79c4bca9ddcc281
风起深空 <15827139224@163.com>
2026-02-10 20:33:57 +0800

    Add files via upload

--- STL_learing/test.cpp
+++ STL_learing/test.cpp
@@ -6,11 +6,21 @@ using namespace std;
 void test_list1()
 {
 	list<int> lt4 = { 1,2,3,4,5,6,7,7,7 };
-	list<int>::iterator it4 = lt4.begin();
-	while (it4 != lt4.end())
+	//list<int>::iterator it4 = lt4.begin();
+	/*while (it4 != lt4.end())
 	{
 		cout << *it4 << " ";
 		++it4;
+	}
+	cout << endl;*/
+
+
+	//把5转移到头部
+	auto it = find(lt4.begin(), lt4.end(), 5);
+	lt4.splice(lt4.begin(), lt4, it);
+	for (auto e : lt4)
+	{
+		cout << e << " ";
 	}
 	cout << endl;
 }
